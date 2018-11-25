3035769c23ef2468be7948463c3d44d8b7eb872b
Xinyu Guan <xguan@andrew.cmu.edu>
2018-11-25 13:26:19 -0500

    dae

---
+++ dae.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import numpy as np
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+from sklearn.preprocessing import StandardScaler
+import pickle as pk
+
+
+def normalize_rows(x):
+  return x / (x.sum(axis=1)[:, np.newaxis])
+
+def make_np_array(y):
+  return np.array([i for i in y])
+
+def getOneHotEncoding(labels):
+  L = labels.unique()
+  return {L[i]: [j==i for j in range(len(L))] for i in range(len(L)) }
+
+def getXandYandDict(filename):
+  # get x and y
+  store = pd.HDFStore(filename)
+  feat_mat_df = store['rpkm']
+  x = feat_mat_df.values
+  labels = store['labels']
+  d = getOneHotEncoding(labels)
+  y = labels.apply(lambda s: d[s]).values
+  assert(x.shape[0] == y.shape[0])
+  return x, y, d
+
+def getXandY(filename, d):
+  # get x and y
+  store = pd.HDFStore(filename)
+  feat_mat_df = store['rpkm']
+  x = feat_mat_df.values
+  labels = store['labels']
+  y = labels.apply(lambda s: d[s]).values
+  assert(x.shape[0] == y.shape[0])
+  return x, y
+
+def main():
+  DATA_DIR = "../data_rpkm/"
+
+  x_train, y_train, d = getXandYandDict(DATA_DIR+'train_data.h5')
+  # x_train, y_train, d = getXandYandDict(DATA_DIR+'all_data.h5')
+
+  y_train = make_np_array(y_train)
+
+  x_test, y_test = getXandY(DATA_DIR+'test_data.h5', d)
+  y_test = make_np_array(y_test)
+
+  noOfTrainingSamples, noOfFeatures = x_train.shape
+  assert(noOfFeatures == x_test.shape[1])
+  noOfClasses = y_train.shape[1]
+  assert(noOfClasses == y_test.shape[1])
+
+  print("Split data")
+  # pre-processing and NN code based on example in
+  # https://medium.com/@pushkarmandot/build-your-first-deep-learning-neural-network-model-using-keras-in-python-a90b5864116d
+
+  sc = StandardScaler()
+  x_train = sc.fit_transform(X = x_train)
+  x_test = sc.transform(X = x_test)
+
+  with open("temp.pk","wb") as f: pk.dump((x_train,x_test),f)
+  # with open("temp.pk","rb") as f: (x_train,x_test) = pk.load(f)
+
+  print(x_train.shape, x_test.shape)
+  print(y_train.shape, y_test.shape)
+
+  print("Normalized features")
+  
+  hiddenNodes = 796
+
+  classifier = Sequential()
+
+  classifier.add(Dense(output_dim = hiddenNodes, input_dim = noOfFeatures,
+    activation = 'tanh', init = 'glorot_uniform'))
+  classifier.add(Dense(output_dim = noOfFeatures, input_dim = hiddenNodes, init = 'glorot_uniform',
+    activation = 'tanh'))
+  sgd = keras.optimizers.SGD(lr=0.1, momentum=0.0, decay=0.0, nesterov=False)
+  classifier.compile(optimizer = sgd, loss = keras.losses.mean_squared_error)
+
+  save_cb = keras.callbacks.ModelCheckpoint("../chkpnts/daeweights.{epoch:02d}-{val_loss:.2f}.hdf5",
+                                              monitor="val_loss", period=5)
+
+  history = classifier.fit(x_train, x_train, batch_size = 10, epochs = 100, verbose = 1, callbacks=[save_cb],
+                            shuffle=1, validation_data = (x_test, x_test))
+  import matplotlib.pyplot as plt
+  # summarize history for loss
+  plt.plot(history.history['loss'])
+  plt.plot(history.history['val_loss'])
+  plt.title('model loss')
+  plt.ylabel('loss')
+  plt.xlabel('epoch')
+  plt.legend(['train', 'test'], loc='upper left')
+  plt.show()
+
+if __name__=="__main__": main()
--- simple_training.py
+++ simple_training.py
@@ -2,124 +2,135 @@ import pandas as pd
 import numpy as np
 
 def normalize_rows(x):
-	return x / (x.sum(axis=1)[:, np.newaxis])
+  return x / (x.sum(axis=1)[:, np.newaxis])
 
 def make_np_array(y):
-	L = []
-	for i in y:
-		L.append(i)
-	L = np.array(L)
-	return L
+  # L = []
+  # for i in y:
+  #   L.append(i)
+  # L = np.array(L)
+  return np.array([i for i in y])
 
-def getDict(labels):
-	L = labels.unique()
-	d = {}
-	for i in range(len(L)):
-		one_hot = [0] * len(L)
-		one_hot[i] = 1
-		d[L[i]] = np.array(one_hot)
-	return d
+def getOneHotEncoding(labels):
+  L = labels.unique()
+  # d = {}
+  # for i in range(len(L)):
+  #   one_hot = [0] * len(L)
+  #   one_hot[i] = 1
+  #   d[L[i]] = np.array(one_hot)
+  return {L[i]: [j==i for j in range(len(L))] for i in range(len(L)) }
 
 def getXandYandDict(filename):
-	# get x and y
-	store = pd.HDFStore(filename)
-	feat_mat_df = store['rpkm']
-	x = feat_mat_df.values
-	labels = store['labels']
-	d = getDict(labels)
-	y = labels.apply(lambda s: d[s]).values
-	assert(x.shape[0] == y.shape[0])
-	return x, y, d
+  # get x and y
+  store = pd.HDFStore(filename)
+  feat_mat_df = store['rpkm']
+  x = feat_mat_df.values
+  labels = store['labels']
+  d = getOneHotEncoding(labels)
+  y = labels.apply(lambda s: d[s]).values
+  assert(x.shape[0] == y.shape[0])
+  return x, y, d
 
-def getXandYwithDict(filename, d):
-	# get x and y
-	store = pd.HDFStore(filename)
-	feat_mat_df = store['rpkm']
-	x = feat_mat_df.values
-	labels = store['labels']
-	y = labels.apply(lambda s: d[s]).values
-	assert(x.shape[0] == y.shape[0])
-	return x, y
+def getXandY(filename, d):
+  # get x and y
+  store = pd.HDFStore(filename)
+  feat_mat_df = store['rpkm']
+  x = feat_mat_df.values
+  labels = store['labels']
+  y = labels.apply(lambda s: d[s]).values
+  assert(x.shape[0] == y.shape[0])
+  return x, y
 
-x_train, y_train, d = getXandYandDict('../ml_10701_ps5_data/train_data.h5')
-print("Normalize training by row")
-x_train = normalize_rows(x_train)
-print(x_train.sum(axis=1))
-y_train = make_np_array(y_train)
+def main():
 
-print(x_train, y_train, d)
+  DATA_DIR = "../data_rpkm/"
 
-x_test, y_test = getXandYwithDict('../ml_10701_ps5_data/test_data.h5', d)
-print("Normalize test by row")
-x_test = normalize_rows(x_test)
-print(x_test.sum(axis=1))
-y_test = make_np_array(y_test)
+  # x_train, y_train, d = getXandYandDict(DATA_DIR+'train_data.h5')
+  x_train, y_train, d = getXandYandDict(DATA_DIR+'train_data.h5')
 
-print(x_test, y_test)
+  # print("Normalize training by row")
+  # x_train = normalize_rows(x_train)
+  # print(x_train.sum(axis=1))
+  y_train = make_np_array(y_train)
 
-#from sklearn.model_selection import train_test_split
-#x_train, x_test, y_train, y_test = train_test_split(x, y,
-#	test_size = 0.2)
+  x_test, y_test = getXandY(DATA_DIR+'test_data.h5', d)
+  y_test = make_np_array(y_test)
+  # import pdb
+  # pdb.set_trace()
 
-noOfTrainingSamples, noOfFeatures = x_train.shape
-assert(noOfFeatures == x_test.shape[1])
-noOfClasses = y_train.shape[1]
-assert(noOfClasses == y_test.shape[1])
+  # print("Normalize test by row")
+  # x_test = normalize_rows(x_test)
+  # print(x_test.sum(axis=1))
+  # y_test = make_np_array(y_test)
+  # from sklearn.model_selection import train_test_split
+  # x_train, x_test, y_train, y_test = train_test_split(x_train, y_train,test_size = 0.2)
 
-print("Split data")
-# pre-processing and NN code based on example in
-# https://medium.com/@pushkarmandot/
-# build-your-first-deep-learning-neural-network-model-using-keras-in-
-# python-a90b5864116d
+  noOfTrainingSamples, noOfFeatures = x_train.shape
+  assert(noOfFeatures == x_test.shape[1])
+  noOfClasses = y_train.shape[1]
+  assert(noOfClasses == y_test.shape[1])
 
-# preprocess x data
+  print("Split data")
+  # pre-processing and NN code based on example in
+  # https://medium.com/@pushkarmandot/
+  # build-your-first-deep-learning-neural-network-model-using-keras-in-
+  # python-a90b5864116d
 
-print(x_train.shape, x_test.shape)
-'''
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-x_train = sc.fit_transform(X = x_train)
-x_test = sc.transform(X = x_test)
+  from sklearn.preprocessing import StandardScaler
+  sc = StandardScaler()
+  x_train = sc.fit_transform(X = x_train)
+  x_test = sc.transform(X = x_test)
 
-print(x_train.shape, x_test.shape)
+  import pickle as pk
+  # with open("temp.pk","wb") as f: pk.dump((x_train,x_test),f)
+  with open("temp.pk","rb") as f: (x_train,x_test) = pk.load(f)
 
-print(x_train, x_test)
-print(type(x_train))
-print(type(x_test))
-#ssert(0)
+  print(x_train.shape, x_test.shape)
+  print(y_train.shape, y_test.shape)
 
-print("Normalized features")
-'''
-import keras
-from keras.models import Sequential
-from keras.layers import Dense
+  print("Normalized features")
+  
+  import keras
+  from keras.models import Sequential
+  from keras.layers import Dense
 
-hiddenNodes = 796
+  hiddenNodes = 796
 
-classifier = Sequential()
+  classifier = Sequential()
 
-classifier.add(Dense(output_dim = hiddenNodes, input_dim = noOfFeatures,
-	activation = 'tanh', init = 'glorot_uniform'))
-classifier.add(Dense(output_dim = noOfClasses, init = 'glorot_uniform',
-	activation = 'softmax'))
+  classifier.add(Dense(output_dim = hiddenNodes, input_dim = noOfFeatures,
+    activation = 'tanh', init = 'glorot_uniform'))
+  # classifier.add(Dense(output_dim = hiddenNodes, input_dim = hiddenNodes,
+  #   activation = 'tanh', init = 'glorot_uniform'))
+  # classifier.add(Dense(output_dim = hiddenNodes, input_dim = hiddenNodes,
+    # activation = 'tanh', init = 'glorot_uniform'))
+  classifier.add(Dense(output_dim = noOfClasses, input_dim = hiddenNodes, init = 'glorot_uniform',
+    activation = 'softmax'))
+  classifier.compile(optimizer = 'sgd', loss = 'categorical_crossentropy',
+    metrics = ['accuracy'])
 
-classifier.compile(optimizer = 'sgd', loss = 'categorical_crossentropy',
-	metrics = ['accuracy'])
+  save_cb = keras.callbacks.ModelCheckpoint("../chkpnts/weights.{epoch:02d}-{val_acc:.2f}.hdf5",
+                                              monitor="val_acc", period=5)
 
-x = classifier.fit(x_train, y_train, batch_size = 10, epochs = 2,
-	verbose = 1 )
-#	validation_data = (x_test, y_test))
+  history = classifier.fit(x_train, y_train, batch_size = 10, epochs = 100, verbose = 1, callbacks=[save_cb],
+                            shuffle=1, validation_data = (x_test, y_test))
+  import matplotlib.pyplot as plt
+  # summarize history for accuracy
+  plt.plot(history.history['acc'])
+  plt.plot(history.history['val_acc'])
+  plt.title('model accuracy')
+  plt.ylabel('accuracy')
+  plt.xlabel('epoch')
+  plt.legend(['train', 'test'], loc='upper left')
+  plt.show()
+  # summarize history for loss
+  plt.plot(history.history['loss'])
+  plt.plot(history.history['val_loss'])
+  plt.title('model loss')
+  plt.ylabel('loss')
+  plt.xlabel('epoch')
+  plt.legend(['train', 'test'], loc='upper left')
+  plt.show()
 
-"Fit training samples to classifier"
-
-loss = classifier.evaluate(x_test, y_test,
-	verbose = 1)
-
-"Evaluated test samples"
-
-print(loss)
-print(classifier.metrics_names)
-
-
-
+if __name__=="__main__": main()
 
