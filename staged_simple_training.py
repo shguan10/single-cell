eff6b17a609b7e1bcfe9a84f819803ac1279c2c0
Xinyu Guan <xguan@andrew.cmu.edu>
2018-12-08 02:45:56 -0500

    cleaned up code

--- staged_simple_training.py
+++ staged_simple_training.py
@@ -28,37 +28,37 @@ all cell_types:
 
 cl_superclass_dict = {
 
-	'CL:0000057 fibroblast': 0,
-	'CL:0000137 osteocyte': 0,
+  'CL:0000057 fibroblast': 0,
+  'CL:0000137 osteocyte': 0,
 
-	'CL:1000497 kidney cell': 1,
-	'CL:0008019 mesenchymal cell': 1,
-	'CL:0002365 medullary thymic epithelial cell' : 1,
+  'CL:1000497 kidney cell': 1,
+  'CL:0008019 mesenchymal cell': 1,
+  'CL:0002365 medullary thymic epithelial cell' : 1,
 
-	'CL:0000163 endocrine cell': 2,
-	'CL:0000169 type B pancreatic cell': 2,
+  'CL:0000163 endocrine cell': 2,
+  'CL:0000169 type B pancreatic cell': 2,
 
-	'CL:0002321 embryonic cell': 3,
-	'CL:0000353 blastoderm cell': 3,
-	'CL:0002322 embryonic stem cell': 3,
+  'CL:0002321 embryonic cell': 3,
+  'CL:0000353 blastoderm cell': 3,
+  'CL:0002322 embryonic stem cell': 3,
 
-	'CL:0000192 smooth muscle cell': 4,
-	'CL:0000746 cardiac muscle cell': 4,
+  'CL:0000192 smooth muscle cell': 4,
+  'CL:0000746 cardiac muscle cell': 4,
 
-	'CL:0001056 dendritic cell, human': 5,
-	'CL:0000084 T cell': 5,
-	'CL:0000235 macrophage': 5,
+  'CL:0001056 dendritic cell, human': 5,
+  'CL:0000084 T cell': 5,
+  'CL:0000235 macrophage': 5,
 
-	'CL:0000081 blood cell': 6,
-	'CL:0000763 myeloid cell': 6,
+  'CL:0000081 blood cell': 6,
+  'CL:0000763 myeloid cell': 6,
 
-	'CL:0002319 neural cell': 7,
-	'CL:0000540 neuron': 7,
-	'CL:0000127 astrocyte': 7,
+  'CL:0002319 neural cell': 7,
+  'CL:0000540 neuron': 7,
+  'CL:0000127 astrocyte': 7,
 
-	'CL:0002034 long term hematopoietic stem cell': 8,
-	'CL:0002033 short term hematopoietic stem cell': 8,
-	'CL:0000037 hematopoietic stem cell': 8,
+  'CL:0002034 long term hematopoietic stem cell': 8,
+  'CL:0002033 short term hematopoietic stem cell': 8,
+  'CL:0000037 hematopoietic stem cell': 8,
 }
 
 cl_cell_superclasses = ['connective tissue', 'other', 'endocrine',
@@ -66,191 +66,218 @@ cl_cell_superclasses = ['connective tissue', 'other', 'endocrine',
     'haem stem']
 
 uberon_classes = {
-	"UBERON:0002048 lung":0,
-	"UBERON:0000115 lung epithelium":0,
+  "UBERON:0002048 lung":0,
+  "UBERON:0000115 lung epithelium":0,
 
-	"UBERON:0000955 brain":1,
-	"UBERON:0002038 substantia nigra":1,
-	"UBERON:0001954 Ammon's horn":1,
-	"UBERON:0001891 midbrain":1,
-	"UBERON:0002435 striatum":1,
-	"UBERON:0001898 hypothalamus":1,
-	"UBERON:0010743 meningeal cluster":1,
+  "UBERON:0000955 brain":1,
+  "UBERON:0002038 substantia nigra":1,
+  "UBERON:0001954 Ammon's horn":1,
+  "UBERON:0001891 midbrain":1,
+  "UBERON:0002435 striatum":1,
+  "UBERON:0001898 hypothalamus":1,
+  "UBERON:0010743 meningeal cluster":1,
 
-	"UBERON:0001003 skin epidermis":2,
-	"UBERON:0001997 olfactory epithelium":2,
-	"UBERON:0001902 epithelium of small intestine":2,
+  "UBERON:0001003 skin epidermis":2,
+  "UBERON:0001997 olfactory epithelium":2,
+  "UBERON:0001902 epithelium of small intestine":2,
 
-	"UBERON:0000473 testis":3,
-	"UBERON:0000992 female gonad":3,
-	"UBERON:0000922 embryo":3,
+  "UBERON:0000473 testis":3,
+  "UBERON:0000992 female gonad":3,
+  "UBERON:0000922 embryo":3,
 
-	"UBERON:0001264 pancreas":4,
-	"UBERON:0000007 pituitary gland":4,
+  "UBERON:0001264 pancreas":4,
+  "UBERON:0000007 pituitary gland":4,
 
-	"UBERON:0000045 ganglion":5,
-	"UBERON:0000044 dorsal root ganglion":5,
-	"UBERON:0004129 growth plate cartilage":5,
+  "UBERON:0000045 ganglion":5,
+  "UBERON:0000044 dorsal root ganglion":5,
+  "UBERON:0004129 growth plate cartilage":5,
 
-	"UBERON:0000966 retina":6,
-	"UBERON:0002107 liver":6,
-	"UBERON:0001851 cortex":6
+  "UBERON:0000966 retina":6,
+  "UBERON:0002107 liver":6,
+  "UBERON:0001851 cortex":6
 }
 
 import pandas as pd
 import numpy as np
 
 def get_cell_superclass(s):
-	if s in cl_superclass_dict:
-		index = cl_superclass_dict[s]
-		arr = np.zeros(9)
-		arr[index] = 1
-		return arr
-	else:
-		index = uberon_classes[s]
-		arr = np.zeros(7)
-		arr[index] = 1
-		return arr
+  if s in cl_superclass_dict:
+    index = cl_superclass_dict[s]
+    arr = np.zeros(9)
+    arr[index] = 1
+    return arr
+  else:
+    index = uberon_classes[s]
+    arr = np.zeros(7)
+    arr[index] = 1
+    return arr
 
 def make_np_array(y):
-	return np.array(list(y))
+  return np.array(list(y))
 
 def getXandYandDict(filename,uberon=False):
-	# get x and y
-	store = pd.HDFStore(filename)
-	feat_mat_df = store['rpkm']
-	#x = feat_mat_df.values
-	labels = store['labels']
+  # get x and y
+  store = pd.HDFStore(filename)
+  feat_mat_df = store['rpkm']
+  #x = feat_mat_df.values
+  labels = store['labels']
 
-	fl = pd.concat([feat_mat_df, pd.DataFrame(labels.rename('labels'))],
-		axis = 1)
+  fl = pd.concat([feat_mat_df, pd.DataFrame(labels.rename('labels'))],
+    axis = 1)
 
-	print(fl['labels'])
-	fl = fl[uberon == fl.labels.str.contains("UBERON")]
+  print(fl['labels'])
+  fl = fl[uberon == fl.labels.str.contains("UBERON")]
 
-	x_df = fl.drop(labels = 'labels', axis=1)
-	print(x_df)
-	x = x_df.values
+  x_df = fl.drop(labels = 'labels', axis=1)
+  print(x_df)
+  x = x_df.values
 
-	y_series = fl['labels']
-	print(y_series)
-	y = y_series.apply(get_cell_superclass)#.values?
-	print(y.value_counts())
+  y_series = fl['labels']
+  print(y_series)
+  y = y_series.apply(get_cell_superclass)#.values?
+  print(y.value_counts())
 
-	print("Got x and y")
+  print("Got x and y")
 
-	return x, y.values
+  return x, y.values
 
-x_train, y_train = getXandYandDict('train_data.h5')
-print("Normalize training by row")
-#x_train = normalize_rows(x_train)
-print(x_train.sum(axis=1))
-y_train = make_np_array(y_train)
+def cacheuberon():
+  x_train, y_train = getXandYandDict('../data_rpkm/train_data.h5',uberon=True)
+  print("Normalize training by row")
+  #x_train = normalize_rows(x_train)
+  print(x_train.sum(axis=1))
+  y_train = make_np_array(y_train)
 
-print(x_train, y_train)
+  print(x_train, y_train)
 
-x_test, y_test = getXandYandDict('test_data.h5')
-print("Normalize test by row")
-#x_test = normalize_rows(x_test)
-print(x_test.sum(axis=1))
-y_test = make_np_array(y_test)
+  x_test, y_test = getXandYandDict('../data_rpkm/test_data.h5',uberon=True)
+  print("Normalize test by row")
+  #x_test = normalize_rows(x_test)
+  print(x_test.sum(axis=1))
+  y_test = make_np_array(y_test)
 
-print(x_test, y_test)
+  print(x_test, y_test)
+
+  # COPY PASTED FROM SIMPLE_TRAINING.PY
+
+  noOfTrainingSamples, noOfFeatures = x_train.shape
+  assert(noOfFeatures == x_test.shape[1])
+  noOfClasses = y_train.shape[1]
+  assert(noOfClasses == y_test.shape[1])
+
+  '''
+  from dim_red_models import *
+  x_train, x_test = myReducedDimMain()
+  '''
+
+  from sklearn.preprocessing import StandardScaler
+  sc = StandardScaler()
+  x_train = sc.fit_transform(X = x_train)
+  x_test = sc.transform(X = x_test)
+
+  print(x_train.shape, x_test.shape)
+
+  print(x_train, x_test)
+  print(type(x_train))
+  print(type(x_test))
+
+  print("Normalized features")
+  import pdb
+  pdb.set_trace()
+  df = pd.DataFrame(data=x_train)
+  df.to_hdf("uberon.h5","x_train")
+  df = pd.DataFrame(data=y_train)
+  df.to_hdf("uberon.h5","y_train")
+
+  df = pd.DataFrame(data=x_test)
+  df.to_hdf("uberon.h5","x_test")
+  df = pd.DataFrame(data=y_test)
+  df.to_hdf("uberon.h5","y_test")
+
+def getuberon():
+  store = pd.HDFStore("uberon.h5")
+  x_train = store["x_train"].values
+  y_train = store["y_train"].values
+  x_test = store["x_test"].values
+  y_test = store["y_test"].values
+  return x_train,y_train,x_test,y_test
+
+def main():
+  x_train,y_train,x_test,y_test = getuberon()
+
+  import keras
+  from keras.models import Sequential
+  from keras.layers import Dense
+  from keras import optimizers
+
+  hiddenNodes = 15
+
+  classifier = Sequential()
+
+  classifier.add(Dense(output_dim = hiddenNodes, input_dim = x_train.shape[1],
+    activation = 'tanh', init = 'glorot_uniform'))
+  #classifier.add(Dense(output_dim = noOfClasses, init = 'glorot_uniform',
+  # activation = 'tanh'))
+  classifier.add(Dense(output_dim = y_train.shape[1], init = 'glorot_uniform',
+    activation = 'softmax'))
+
+  # sgd = optimizers.SGD(lr = 0.1, decay = 1e-6, momentum = 0.9, nesterov = True)
+  classifier.compile(optimizer = 'sgd', loss = 'categorical_crossentropy',
+    metrics = ['accuracy'])
+
+  savecb = keras.callbacks.ModelCheckpoint(filepath='../chkpnts/uberon-{val_acc:.2f}.hdf5', monitor='val_acc', verbose=1, save_best_only=True)
+
+  x = classifier.fit(x_train, y_train, batch_size = 100, epochs = 100,
+    verbose = 1, validation_data = (x_test, y_test), callbacks=[savecb])
+  # while True:
+  #   session = keras.get_session()
+  #   initial_weights = classifier.get_weights()
+  #   new_weights = [keras.initializers.glorot_uniform()(w.shape).eval(session=session) for w in initial_weights]
+  #   classifier.set_weights(new_weights)
+
+  loss = classifier.evaluate(x_test, y_test,
+    verbose = 1)
+
+  # "Evaluated test samples"
+
+  print(loss)
+  print(classifier.metrics_names)
+
+  y_pred = classifier.predict(x_test)
+
+  from sklearn import metrics
+  matrix = metrics.confusion_matrix(y_test.argmax(axis=1), y_pred.argmax(axis=1))
+  np.set_printoptions(threshold = np.nan)
+  print(matrix)
+
+  matrix = []
+  y_pred = np.argmax(y_pred, axis = 1)
+  y_test = np.argmax(y_test, axis = 1)
+  for i in set(y_pred):
+    false_positives, false_negatives = 0, 0
+    true_positives = 0
+    for j in range(len(y_test)):
+      if (y_pred[j] == i) and (y_test[j] == i):
+        true_positives += 1
+      elif (y_pred[j] == i) and not (y_test[j] == i):
+        false_positives += 1
+      elif (not (y_pred[j] == i)) and (y_test[j] == i):
+        false_negatives += 1
+    total_no = true_positives + false_negatives
+    if total_no != 0:
+      matrix.append((i, total_no, true_positives / total_no,
+        false_positives / total_no, false_negatives / total_no))
+
+  for x in matrix:
+    print(x)
+  '''
+  d_new = {}
+  for i in range(len(d)):
+    d_new[i] = np.argmax(d[i])
+  print(d_new)
+  '''
 
 
-
-# COPY PASTED FROM SIMPLE_TRAINING.PY
-
-noOfTrainingSamples, noOfFeatures = x_train.shape
-assert(noOfFeatures == x_test.shape[1])
-noOfClasses = y_train.shape[1]
-assert(noOfClasses == y_test.shape[1])
-
-'''
-from dim_red_models import *
-x_train, x_test = myReducedDimMain()
-'''
-
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-x_train = sc.fit_transform(X = x_train)
-x_test = sc.transform(X = x_test)
-
-print(x_train.shape, x_test.shape)
-
-print(x_train, x_test)
-print(type(x_train))
-print(type(x_test))
-#ssert(0)
-print("Normalized features")
-
-import keras
-from keras.models import Sequential
-from keras.layers import Dense
-from keras import optimizers
-
-hiddenNodes = 15
-
-classifier = Sequential()
-
-classifier.add(Dense(output_dim = hiddenNodes, input_dim = noOfFeatures,
-	activation = 'tanh', init = 'glorot_uniform'))
-#classifier.add(Dense(output_dim = noOfClasses, init = 'glorot_uniform',
-#	activation = 'tanh'))
-classifier.add(Dense(output_dim = noOfClasses, init = 'glorot_uniform',
-	activation = 'softmax'))
-
-# sgd = optimizers.SGD(lr = 0.1, decay = 1e-6, momentum = 0.9, nesterov = True)
-classifier.compile(optimizer = 'sgd', loss = 'categorical_crossentropy',
-	metrics = ['accuracy'])
-
-x = classifier.fit(x_train, y_train, batch_size = 10, epochs = 5,
-	verbose = 1 )
-#	validation_data = (x_test, y_test))
-
-"Fit training samples to classifier"
-
-loss = classifier.evaluate(x_test, y_test,
-	verbose = 1)
-
-"Evaluated test samples"
-
-print(loss)
-print(classifier.metrics_names)
-
-y_pred = classifier.predict(x_test)
-
-from sklearn import metrics
-matrix = metrics.confusion_matrix(y_test.argmax(axis=1), y_pred.argmax(axis=1))
-np.set_printoptions(threshold = np.nan)
-print(matrix)
-
-matrix = []
-y_pred = np.argmax(y_pred, axis = 1)
-y_test = np.argmax(y_test, axis = 1)
-for i in set(y_pred):
-	false_positives, false_negatives = 0, 0
-	true_positives = 0
-	for j in range(len(y_test)):
-		if (y_pred[j] == i) and (y_test[j] == i):
-			true_positives += 1
-		elif (y_pred[j] == i) and not (y_test[j] == i):
-			false_positives += 1
-		elif (not (y_pred[j] == i)) and (y_test[j] == i):
-			false_negatives += 1
-	total_no = true_positives + false_negatives
-	if total_no != 0:
-		matrix.append((i, total_no, true_positives / total_no,
-			false_positives / total_no, false_negatives / total_no))
-
-for x in matrix:
-	print(x)
-'''
-d_new = {}
-for i in range(len(d)):
-	d_new[i] = np.argmax(d[i])
-print(d_new)
-'''
-
-
-
+if __name__ == '__main__':
+  # cacheuberon()
+  main()
